282a8727680872a05d99d5688d8f431f775c0426
Egeis <no-reply@blocklands.net>
2014-06-25 20:44:51 -0700

    Initial Commit

---
+++ Assets/Scripts/controller_GUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller_GUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnGUI() {
+		string[] toolbar = {"Clear", "Random", "Acorn"};
+		
+		GUILayout.BeginArea( new Rect(0,0, (Screen.width), (Screen.height/4) ) );
+		GUILayout.BeginHorizontal ();
+		
+		GUILayout.Toolbar(1, toolbar);
+		
+		GUILayout.EndHorizontal ();
+		GUILayout.EndArea ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
